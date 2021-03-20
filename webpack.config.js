6ea992b5b52a3404885eede61e51c5a4e7bee886
gholib <gtrandnet@gmail.com>
2021-03-21 03:34:16 +0500

    header component@

--- webpack.config.js
+++ webpack.config.js
@@ -93,6 +93,18 @@ module.exports = {
       {
         test: /\.(png|jpg|svg)$/,
         loader: 'url-loader'
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
     ]
   }
