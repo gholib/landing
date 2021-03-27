216845bfb7f8a8db2990255df822315bd03e31b3
gholib <gtrandnet@gmail.com>
2021-03-28 02:59:07 +0500

    responsive menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './scss/index.scss'
 
-console.log('Working!')
+const navbar = document.getElementById('navbar')
+const mobiNavbar = document.getElementById('navbarSupportedContent')
+const navbarToggler = document.getElementById('navbar-toggler')
+
+setBackgroundToNavbar(navbar)
+
+navbarToggler.addEventListener('click', function() {
+    toggleMobiNavbar()
+})
+window.addEventListener('scroll', function() {
+    setBackgroundToNavbar(navbar)
+})
+
+function setBackgroundToNavbar(navbar) {
+    if (pageYOffset > 10) {
+        navbar.classList.add('affix')
+    }else {
+        navbar.classList.remove('affix')
+    }
+}
+
+function toggleMobiNavbar() {
+    mobiNavbar.classList.toggle('show')
+}
--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,7 @@ module.exports = {
       {
         from: path.resolve(__dirname, 'src/assets'),
         to: path.resolve(__dirname, 'dist/assets')
-      }
+      },
     ]),
     new MiniCssExtractPlugin({
       filename: filename('css')
